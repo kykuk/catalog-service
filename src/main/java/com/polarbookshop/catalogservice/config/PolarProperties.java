6dd3eae230a7347c6c1ac4c01687e12c754ff0dd
kykuk <kykuk@cyberlogitec.com>
2023-02-03 16:23:49 +0900

    Remove Lombok

--- src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
+++ src/main/java/com/polarbookshop/catalogservice/config/PolarProperties.java
@@ -3,12 +3,16 @@ package com.polarbookshop.catalogservice.config;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
 @Configuration
 @ConfigurationProperties(prefix = "polar")
 public class PolarProperties {
 	private String greeting;
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
 }
